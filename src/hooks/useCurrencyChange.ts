2fbe4384e7d1cbe30f59bfaad6c84370cd52e9be
Ivan <ivanbelasich@gmail.com>
2023-07-25 14:49:27 -0300

    Remove currency type from useCurrencyChange hook

--- src/hooks/useCurrencyChange.ts
+++ src/hooks/useCurrencyChange.ts
@@ -1,26 +1,18 @@
 import { useState } from 'react';
 
-import { Balance } from '@/types/balance.types';
+import { Balance } from '@/types/balance.type';
 
 const useCurrencyChange = (items: Balance[]) => {
   const [currency, setCurrency] = useState('');
   const [selectedBalance, setSelectedBalance] = useState<Balance | null>(null);
-  const [amountType, setAmountType] = useState('');
 
   const handleCurrencyChange = (value: any) => {
     setCurrency(value);
     const balance = items.find((bal: any) => bal.value === value);
     setSelectedBalance(balance as any);
-    if (value === 'BRL') {
-      setAmountType('R$:');
-    } else if (value === 'USDC') {
-      setAmountType('$US:');
-    } else {
-      setAmountType('');
-    }
   };
 
-  return { currency, selectedBalance, amountType, setCurrency, handleCurrencyChange };
+  return { currency, selectedBalance, setCurrency, handleCurrencyChange };
 };
 
 export default useCurrencyChange;
