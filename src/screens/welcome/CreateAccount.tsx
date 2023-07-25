d62fa9d90cca917f634db1d19f6a4856626283ae
Ivan <ivanbelasich@gmail.com>
2023-07-25 14:54:38 -0300

    Add new fields to CreateAccount Screen

--- src/screens/welcome/CreateAccount.tsx
+++ src/screens/welcome/CreateAccount.tsx
@@ -10,34 +10,68 @@ type SignUpProps = {
 
 const StyledView = styled(View);
 const StyledText = styled(Text);
+const StyledTextInput = styled(TextInput);
 
 const CreateAccount = ({ navigation }: SignUpProps) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [location, setLocation] = useState('');
   const [error, setError] = useState('');
 
   const handleSubmit = async () => {
     try {
-      SignUp(email, password);
+      await SignUp(email, password, firstName, lastName, location);
+      navigation.navigate('Login');
     } catch (error) {
       console.error(error);
+      setError('Error during sign up. Please try again.');
     }
   };
 
   return (
-    <StyledView className="gap-2 m-0">
-      <StyledView className="gap-2">
-        <TextInput placeholder="Email" value={email} onChangeText={setEmail} />
-        <TextInput placeholder="Password" value={password} onChangeText={setPassword} secureTextEntry />
-      </StyledView>
-      <StyledView className="bg-red rounded-md mx-6 h-10 justify-center">
-        <TouchableOpacity onPress={handleSubmit}>
-          <StyledText className="text-white text-center text-md">Sign Up</StyledText>
-        </TouchableOpacity>
-      </StyledView>
-      <StyledView className="bg-blue rounded-md mx-6 h-10 justify-center">
-        <TouchableOpacity onPress={() => navigation.navigate('LogIn')}>
-          <StyledText className="text-white text-center text-md">Already have an account? Log in</StyledText>
+    <StyledView className="gap-4 p-6 mt-6">
+      <StyledTextInput
+        className="text-lg bg-white h-10 p-2 rounded-sm mb-2"
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+      />
+      <StyledTextInput
+        className="text-lg bg-white h-10 p-2 rounded-sm mb-2"
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      <StyledTextInput
+        className="text-lg bg-white h-10 p-2 rounded-sm mb-2"
+        placeholder="First Name"
+        value={firstName}
+        onChangeText={setFirstName}
+      />
+      <StyledTextInput
+        className="text-lg bg-white h-10 p-2 rounded-sm mb-2"
+        placeholder="Last Name"
+        value={lastName}
+        onChangeText={setLastName}
+      />
+      <StyledTextInput
+        className="text-lg bg-white h-10 p-2 rounded-sm mb-2"
+        placeholder="Location"
+        value={location}
+        onChangeText={setLocation}
+      />
+      <TouchableOpacity onPress={handleSubmit}>
+        <StyledView className="bg-red rounded-md h-12 justify-center">
+          <StyledText className="text-white text-center text-lg">Sign Up</StyledText>
+        </StyledView>
+      </TouchableOpacity>
+      <StyledView className="flex-row justify-center items-center gap-2">
+        <StyledText className="text-lg">Already have an account?</StyledText>
+        <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+          <StyledText className="text-blue text-xl font-bold">Log in</StyledText>
         </TouchableOpacity>
       </StyledView>
       {error ? <Text>{error}</Text> : null}
