b0f048b19e96b2968f87c20fe520992621a23eb8
Ivan <ivanbelasich@gmail.com>
2023-07-25 15:14:45 -0300

    Add emigro and profile images

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,6 +1,4 @@
-import { useAsyncStorage } from '@react-native-async-storage/async-storage';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { useEffect, useState } from 'react';
 
 import { BottomTabNavigator } from './BottomTabNavigator';
 
@@ -12,7 +10,7 @@ import { Welcome } from '@screens/welcome/Welcome';
 type RootStackParamList = {
   Welcome: undefined;
   SignUp: undefined;
-  LogIn: undefined;
+  Login: undefined;
   Root: undefined;
   ConfirmPayment: undefined;
   Wallet: undefined;
@@ -21,28 +19,13 @@ type RootStackParamList = {
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function RootNavigator() {
-  const { getItem } = useAsyncStorage('authToken');
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
-
-  useEffect(() => {
-    const checkAuthentication = async () => {
-      const token = await getItem();
-      if (token) {
-        setIsLoggedIn(true);
-      } else {
-        setIsLoggedIn(false);
-      }
-    };
-    checkAuthentication();
-  }, []);
-
   return (
-    <Stack.Navigator initialRouteName={isLoggedIn ? 'Root' : 'Welcome'}>
-      <Stack.Screen name="Welcome" component={Welcome} options={{ headerShown: false }} />
+    <Stack.Navigator initialRouteName={'Welcome'}>
       <Stack.Screen name="Root" component={BottomTabNavigator} options={{ headerShown: false }} />
-      <Stack.Screen name="SignUp" component={CreateAccount} options={{ headerTitle: 'Sign Up' }} />
-      <Stack.Screen name="LogIn" component={Login} options={{ headerTitle: 'Log In' }} />
       <Stack.Screen name="ConfirmPayment" component={ConfirmPayment} options={{ headerShown: false }} />
+      <Stack.Screen name="Welcome" component={Welcome} options={{ headerShown: false }} />
+      <Stack.Screen name="SignUp" component={CreateAccount} options={{ headerTitle: 'Sign Up' }} />
+      <Stack.Screen name="Login" component={Login} options={{ headerTitle: 'Log In' }} />
     </Stack.Navigator>
   );
 }
