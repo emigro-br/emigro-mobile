b8e65ea013cc40b8207bdad36ed2494b1e6fb4aa
gabicasas01 <casas.gf7@gmail.com>
2023-10-09 18:33:38 -0300

    Delete unnecessary files

--- src/api/userPool.ts
+++
@@ -1,8 +0,0 @@
-import { CognitoUserPool } from 'amazon-cognito-identity-js';
-
-const userPool = new CognitoUserPool({
-  UserPoolId: process.env.AWS_COGNITO_USER_POOL_ID || '',
-  ClientId: process.env.AWS_COGNITO_CLIENT_ID || '',
-});
-
-export default userPool;
