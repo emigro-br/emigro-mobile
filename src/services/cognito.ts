b35168e40c29123a0534713221ef871ed7bbc916
gabicasas01 <casas.gf7@gmail.com>
2023-10-09 18:29:57 -0300

    Add cognito service, to log in and create a user

---
+++ src/services/cognito.ts
@@ -0,0 +1,52 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import { SIGNIN_ERROR_MESSAGE, SIGNUP_ERROR_MESSAGE } from '@constants/errorMessages';
+
+const BACKEND_URL = process.env.BACKEND_URL;
+
+export const signIn = async (email: string, password: string, role: string) => {
+  const signInUrl = `${BACKEND_URL}/auth/login`;
+
+  try {
+    const response = await fetch(signInUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email,
+        password,
+        role,
+      }),
+    });
+
+    const signInResponse = await response.json();
+    const { accessToken, refreshToken, idToken } = signInResponse;
+
+    const session = {
+      accessToken,
+      refreshToken,
+      idToken,
+      email,
+    };
+    await AsyncStorage.setItem('session', JSON.stringify(session));
+  } catch (error) {
+    console.error(SIGNIN_ERROR_MESSAGE, error);
+  }
+};
+
+export const signUp = async (registerUser: IRegisterUser) => {
+  const registerUrl = `${BACKEND_URL}/auth/register`;
+  try {
+    const response = await fetch(registerUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(registerUser),
+    });
+    return await response.json();
+  } catch (error) {
+    console.error(SIGNUP_ERROR_MESSAGE, error);
+  }
+};
