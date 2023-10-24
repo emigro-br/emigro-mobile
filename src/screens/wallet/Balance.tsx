a192cbf15d898f5c16e0a69a482b9fffe8917f85
Ivan <ivanbelasich@gmail.com>
2023-10-24 20:10:37 -0300

    Add message to Balance when there are not founds

--- src/screens/wallet/Balance.tsx
+++ src/screens/wallet/Balance.tsx
@@ -74,25 +74,25 @@ const Balance: React.FunctionComponent = () => {
         withdrawLoading={withdrawLoading}
       />
       {userBalance?.map(({ balance, assetCode }, index) => {
-        if (Number(balance) > 0) {
-          return (
-            <StyledView
-              key={index}
-              className="flex-row h-20 items-center justify-between m-1 px-6 w-full bg-white rounded"
-            >
-              <StyledView className="flex-row gap-2">
-                {assetCode === AssetCode.BRL && <Image source={brlLogo} style={{ width: 30, height: 30 }} />}
-                {assetCode === AssetCode.USDC && <Image source={usdLogo} style={{ width: 30, height: 30 }} />}
-                <StyledText className="font-bold text-xl">
-                  {assetCode === AssetCode.USDC ? AssetCode.USD : assetCode}
-                </StyledText>
-              </StyledView>
-              <StyledText className="text-xl">{Number(balance)}</StyledText>
+        return (
+          <StyledView
+            key={index}
+            className="flex-row h-20 items-center justify-between m-1 px-6 w-full bg-white rounded"
+          >
+            <StyledView className="flex-row gap-2">
+              {assetCode === AssetCode.BRL && <Image source={brlLogo} style={{ width: 30, height: 30 }} />}
+              {assetCode === AssetCode.USDC && <Image source={usdLogo} style={{ width: 30, height: 30 }} />}
+              <StyledText className="font-bold text-xl">
+                {assetCode === AssetCode.USDC ? AssetCode.USD : assetCode}
+              </StyledText>
             </StyledView>
-          );
-        } else {
-          return null;
-        }
+            {Number(balance) > 0 ? (
+              <StyledText className="text-xl">{Number(balance)}</StyledText>
+            ) : (
+              <StyledText className="font-bold text-xl">No funds</StyledText>
+            )}
+          </StyledView>
+        );
       })}
     </StyledView>
   );
