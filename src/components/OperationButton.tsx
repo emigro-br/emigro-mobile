754ff2c3c64adb44f45875d9f6fe8261bb350216
gabicasas01 <casas.gf7@gmail.com>
2023-11-03 20:00:35 -0300

    Add OperationButton.tsx to Operation.tsx

---
+++ src/components/OperationButton.tsx
@@ -0,0 +1,30 @@
+import Button from '@components/Button';
+import { OperationType } from '@constants/constants';
+import { styled } from 'nativewind';
+import { FunctionComponent } from 'react';
+import { View } from 'react-native';
+
+const StyledView = styled(View);
+
+export interface OperationButtonProps {
+    onPress: (operation: OperationType) => void;
+}
+
+const OperationButton: FunctionComponent<OperationButtonProps> = ({ onPress }) => {
+        const handleOnPress = (operationType: OperationType) => {
+            onPress(operationType);
+        };
+        
+    return (
+        <StyledView>
+            <Button textColor="white" backgroundColor="red" onPress={() => handleOnPress(OperationType.DEPOSIT)}>
+                {OperationType.DEPOSIT}
+            </Button>
+            <Button textColor="white" backgroundColor="red" onPress={() => handleOnPress(OperationType.WITHDRAW)}>
+                {OperationType.WITHDRAW}
+            </Button>
+        </StyledView>
+    );
+};
+
+export default OperationButton
