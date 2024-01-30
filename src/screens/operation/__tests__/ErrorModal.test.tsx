ce640e213a7d423709b2e50dcc4ddcaaa9d22bb4
Osvaldo Matos Junior <tupy@riachao.com>
2024-01-30 16:26:19 -0300

    Adjust identation

--- src/screens/operation/__tests__/ErrorModal.test.tsx
+++ src/screens/operation/__tests__/ErrorModal.test.tsx
@@ -3,16 +3,16 @@ import { render, fireEvent } from '@testing-library/react-native';
 import { ErrorModal } from '../modals/ErrorModal';
 
 describe('ErrorModal', () => {
-	it('renders correctly', () => {
-		const { getByText } = render(<ErrorModal isVisible={true} onClose={jest.fn()} errorMessage="Test error message" />);
-		expect(getByText('Transaction Failed')).toBeTruthy();
-		expect(getByText('Test error message')).toBeTruthy();
-	});
+  it('renders correctly', () => {
+    const { getByText } = render(<ErrorModal isVisible={true} onClose={jest.fn()} errorMessage="Test error message" />);
+    expect(getByText('Transaction Failed')).toBeTruthy();
+    expect(getByText('Test error message')).toBeTruthy();
+  });
 
-	it('calls onClose when the Close button is pressed', () => {
-		const onClose = jest.fn();
-		const { getByText } = render(<ErrorModal isVisible={true} onClose={onClose} errorMessage="Test error message" />);
-		fireEvent.press(getByText('Close'));
-		expect(onClose).toHaveBeenCalled();
-	});
+  it('calls onClose when the Close button is pressed', () => {
+    const onClose = jest.fn();
+    const { getByText } = render(<ErrorModal isVisible={true} onClose={onClose} errorMessage="Test error message" />);
+    fireEvent.press(getByText('Close'));
+    expect(onClose).toHaveBeenCalled();
+  });
 });
--- src/screens/operation/__tests__/LoadingModal.test.tsx
+++ src/screens/operation/__tests__/LoadingModal.test.tsx
@@ -3,13 +3,13 @@ import { render } from '@testing-library/react-native';
 import { LoadingModal } from '../modals/LoadingModal';
 
 describe('LoadingModal', () => {
-	it('renders correctly with default label', () => {
-		const { getByText } = render(<LoadingModal isVisible={true} />);
-		expect(getByText('Loading...')).toBeTruthy();
-	});
+  it('renders correctly with default label', () => {
+    const { getByText } = render(<LoadingModal isVisible={true} />);
+    expect(getByText('Loading...')).toBeTruthy();
+  });
 
-	it('renders correctly with custom label', () => {
-		const { getByText } = render(<LoadingModal isVisible={true} label="Custom label" />);
-		expect(getByText('Custom label')).toBeTruthy();
-	});
+  it('renders correctly with custom label', () => {
+    const { getByText } = render(<LoadingModal isVisible={true} label="Custom label" />);
+    expect(getByText('Custom label')).toBeTruthy();
+  });
 });
