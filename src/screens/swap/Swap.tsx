644bd7427063988fb0b177aa4541d2b7e2b70523
Osvaldo Matos Junior <tupy@riachao.com>
2024-02-02 12:28:55 -0300

    Switch sell or buy when tap in the arrow

--- src/screens/swap/Swap.tsx
+++ src/screens/swap/Swap.tsx
@@ -1,10 +1,10 @@
 
 import React, { useEffect, useState } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import Button from '@/components/Button';
 import { AssetCode } from '@constants/assetCode';
 import { styled } from 'nativewind';
-import { ArrowDownCircleIcon } from 'react-native-heroicons/solid';
+import { ArrowPathIcon } from 'react-native-heroicons/solid';
 import { AssetSwap } from './AssetSwap';
 import { SwapType } from './types';
 import BalanceStore from '@/stores/BalanceStore';
@@ -22,6 +22,7 @@ export const Swap = () => {
    const [buyValue, setBuyValue] = useState(0);
    const [rate, setRate] = useState<number | null>(null);
 
+   // TODO: disable the not active input while fetching the rate
    const fetchRate = async () => {
       const data: IQuoteRequest = {
          from: sellAsset,
@@ -42,9 +43,43 @@ export const Swap = () => {
    const onChangeValue = (value: number, type: SwapType) => {
       if (!rate) return;
       if (type === SwapType.SELL) {
-         setBuyValue(value * rate);
+         setSellValue(value);
       } else {
-         setSellValue(value / rate);
+         setBuyValue(value);
+      }
+   }
+
+   useEffect(() => {
+      if (!rate) return;
+
+      const calculateNewSellValue = (buyValue: number) => buyValue / rate
+      const calculateNewBuyValue = (sellValue: number) => sellValue * rate
+
+      switch (active) {
+         case SwapType.SELL:
+            // Update buyValue based on sellValue
+            const newBuyValue = calculateNewBuyValue(sellValue);
+            setBuyValue(newBuyValue);
+            break;
+         case SwapType.BUY:
+            // Update sellValue based on buyValue
+            const newSellValue = calculateNewSellValue(buyValue);
+            setSellValue(newSellValue)
+            break;
+         default:
+            break;
+      }
+   }, [sellValue, buyValue, rate]);
+
+   const handleSwap = () => {
+      const temp = sellAsset;
+      setSellAsset(buyAsset);
+      setBuyAsset(temp);
+
+      if (active === SwapType.SELL) {
+         setSellValue(sellValue);
+      } else {
+         setBuyValue(buyValue);
       }
    }
 
@@ -55,26 +90,28 @@ export const Swap = () => {
    return (
       <StyledView className='h-full p-4'>
          <StyledText className='text-2xl font-bold mb-4'>Sell {sellAsset}</StyledText>
-         <AssetSwap 
-            asset={sellAsset} 
-            balance={BalanceStore.get(sellAsset)} 
-            sellOrBuy={SwapType.SELL} 
-            value={sellValue} 
-            onChangeAsset={setSellAsset} 
-            onChangeValue={onChangeValue} 
+         <AssetSwap
+            asset={sellAsset}
+            balance={BalanceStore.get(sellAsset)}
+            sellOrBuy={SwapType.SELL}
+            value={sellValue}
+            onChangeAsset={setSellAsset}
+            onChangeValue={onChangeValue}
             isActive={active === SwapType.SELL}
             onPress={() => setActive(SwapType.SELL)}
          />
-         <StyledView className='items-center mb-4' testID='arrowIcon'>
-            <ArrowDownCircleIcon size={36} color='red' />
-         </StyledView>
-         <AssetSwap 
-            asset={buyAsset} 
-            balance={BalanceStore.get(buyAsset)} 
-            sellOrBuy={SwapType.BUY} 
-            value={buyValue} 
-            onChangeAsset={setBuyAsset} 
-            onChangeValue={onChangeValue} 
+         <TouchableOpacity onPress={handleSwap}>
+            <StyledView className='items-center mb-4' testID='arrowIcon'>
+               <ArrowPathIcon size={24} color='red' />
+            </StyledView>
+         </TouchableOpacity>
+         <AssetSwap
+            asset={buyAsset}
+            balance={BalanceStore.get(buyAsset)}
+            sellOrBuy={SwapType.BUY}
+            value={buyValue}
+            onChangeAsset={setBuyAsset}
+            onChangeValue={onChangeValue}
             isActive={active === SwapType.BUY}
             onPress={() => setActive(SwapType.BUY)}
          />
