21fbc8eb5b1d3f69f4356722ad965ee75fedc68e
Osvaldo Matos Junior <tupy@riachao.com>
2024-03-01 16:31:02 -0300

    [gluestack] Wallet

--- src/components/Balance.tsx
+++ src/components/Balance.tsx
@@ -1,42 +1,40 @@
-import { Image, Text, View } from 'react-native';
-
-import { styled } from 'nativewind';
+import { Avatar, AvatarImage, Card, HStack, Heading, Text, VStack } from '@gluestack-ui/themed';
 
 import { IBalance } from '@/types/IBalance';
 import { CryptoAsset } from '@/types/assets';
 
 import { AssetToName, AssetToSymbol, iconFor } from '@utils/assets';
 
-const StyledView = styled(View);
-const StyledText = styled(Text);
-
-interface BalanceProps {
+interface Props {
   userBalance: IBalance[];
 }
 
-const Balance: React.FunctionComponent<BalanceProps> = ({ userBalance }) => {
+const Balance: React.FC<Props> = ({ userBalance }) => {
   return (
-    <StyledView className="flex items-center h-full">
-      <StyledText className="w-full text-left text-xl font-medium mb-2">Accounts</StyledText>
+    <VStack space="sm">
+      <Heading>Accounts</Heading>
       {userBalance?.map(({ balance, assetCode, assetType }, index) => {
         const asset: CryptoAsset =
           assetType === 'native' ? CryptoAsset.XLM : CryptoAsset[assetCode as keyof typeof CryptoAsset];
         return (
-          <StyledView
-            key={index}
-            className="flex-row items-center justify-between h-16 m-1 px-3 w-full border border-slate-300 bg-white rounded-xl"
-          >
-            <StyledView className="flex-row gap-2">
-              <Image source={iconFor(asset)} style={{ width: 30, height: 30 }} />
-              <StyledText className="font-bold text-lg">{AssetToName[asset]}</StyledText>
-            </StyledView>
-            <StyledText className="text-base">
-              {AssetToSymbol[asset]} {Number(balance).toFixed(2)}
-            </StyledText>
-          </StyledView>
+          <Card key={index} size="lg" variant="outline">
+            <HStack justifyContent="space-between">
+              <HStack space="md">
+                <Avatar size="sm" bg="$transparent">
+                  <AvatarImage source={iconFor(asset)} alt={AssetToName[asset]} />
+                </Avatar>
+                <Text bold size="lg">
+                  {AssetToName[asset]}
+                </Text>
+              </HStack>
+              <Text>
+                {AssetToSymbol[asset]} {Number(balance).toFixed(2)}
+              </Text>
+            </HStack>
+          </Card>
         );
       })}
-    </StyledView>
+    </VStack>
   );
 };
 
--- src/screens/wallet/Wallet.tsx
+++ src/screens/wallet/Wallet.tsx
@@ -1,18 +1,15 @@
 import React, { useState } from 'react';
-import { RefreshControl, ScrollView, View } from 'react-native';
+import { RefreshControl } from 'react-native';
 
 import { useFocusEffect } from '@react-navigation/native';
 
+import { Box, ScrollView, VStack } from '@gluestack-ui/themed';
 import { observer } from 'mobx-react-lite';
-import { styled } from 'nativewind';
 
 import Balance from '@components/Balance';
 import OperationButtons from '@components/OperationButtons';
 
 import { balanceStore } from '@stores/BalanceStore';
-
-const StyledScrollView = styled(ScrollView);
-const StyledView = styled(View);
 
 const Wallet: React.FC = observer(() => {
   const [refreshing, setRefreshing] = useState(false);
@@ -33,19 +30,17 @@ const Wallet: React.FC = observer(() => {
   }, [balanceStore.fetchUserBalance]);
 
   return (
-    <StyledScrollView
-      className="flex-1 bg-white"
+    <ScrollView
+      bg="$white"
       refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} title="Refreshing..." />}
     >
-      <StyledView className="flex">
-        <StyledView className="px-4 py-8">
+      <VStack p="$4" space="lg">
+        <Box py="$8">
           <OperationButtons />
-        </StyledView>
-        <StyledView className="px-4 w-full">
-          <Balance userBalance={balanceStore.userBalance} />
-        </StyledView>
-      </StyledView>
-    </StyledScrollView>
+        </Box>
+        <Balance userBalance={balanceStore.userBalance} />
+      </VStack>
+    </ScrollView>
   );
 });
 
