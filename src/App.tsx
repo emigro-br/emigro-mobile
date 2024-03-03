1e8ecb472de0cd0999cdf2a90e5609e3e57236bb
Osvaldo Matos Junior <tupy@riachao.com>
2024-03-03 13:08:25 -0300

    Organize App entrypoint

---
+++ src/App.tsx
@@ -0,0 +1,65 @@
+import { useEffect, useState } from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+import { NavigationContainer } from '@react-navigation/native';
+
+import { GluestackUIProvider } from '@gluestack-ui/themed';
+import { StatusBar } from 'expo-status-bar';
+import { observer } from 'mobx-react-lite';
+
+import { VendorContextProvider } from '@/contexts/VendorContext';
+
+import { config } from '@config/gluestack-ui.config';
+
+import RootNavigator from '@navigation/index';
+
+import { SplashScreen } from '@screens/Splash';
+
+import { sessionStore } from '@stores/SessionStore';
+
+export default observer(function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const bootstrapAsync = async () => {
+    try {
+      const authSession = await sessionStore.load();
+      if (authSession) {
+        // always refresh the session on app start
+        console.debug('Refreshing session...');
+        const newSession = await sessionStore.refresh();
+        if (!newSession) {
+          throw new Error('Can not refresh the session');
+        }
+      }
+    } catch (error) {
+      console.warn('Can not load the token, cleaning session', error);
+      await sessionStore.clear();
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    bootstrapAsync();
+  }, []);
+
+  if (isLoading) {
+    // We haven't finished checking for the token yet
+    return <SplashScreen />;
+  }
+
+  const isSignedIn = !!sessionStore.session;
+
+  return (
+    <GluestackUIProvider config={config}>
+      <SafeAreaProvider>
+        <VendorContextProvider>
+          <StatusBar style="dark" />
+          <NavigationContainer>
+            <RootNavigator isSignedIn={isSignedIn} />
+          </NavigationContainer>
+        </VendorContextProvider>
+      </SafeAreaProvider>
+    </GluestackUIProvider>
+  );
+});
