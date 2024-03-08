45a7274287eb07ac14582b3d8757063f45164ff0
Osvaldo Matos Junior <tupy@riachao.com>
2024-03-08 17:13:42 -0300

    AssetAvatar sizes

--- src/components/AssetAvatar.tsx
+++ src/components/AssetAvatar.tsx
@@ -6,12 +6,13 @@ import { iconFor, labelFor } from '@utils/assets';
 
 type Props = {
   item: CryptoOrFiat;
+  size: 'xs' | 'sm' | 'md' | 'lg';
   testID?: string;
 };
 
-export const AssetAvatar = ({ item, testID = 'asset-avatar' }: Props) => {
+export const AssetAvatar = ({ item, size = 'md', testID = 'asset-avatar' }: Props) => {
   return (
-    <Avatar size="md" bg="$transparent" testID={testID}>
+    <Avatar size={size} bg="$transparent" testID={testID}>
       <AvatarImage source={iconFor(item)} alt={labelFor(item)} />
     </Avatar>
   );
