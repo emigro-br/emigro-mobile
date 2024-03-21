0caff61c843d88d94c62f7657b9ee4e130958c53
Osvaldo Matos Junior <tupy@riachao.com>
2024-03-21 13:25:38 -0300

    Adjust Header padding top for devices with notch

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 import { useNavigation } from '@react-navigation/native';
 
@@ -7,10 +8,12 @@ import { Box, Center, Image, Pressable } from '@gluestack-ui/themed';
 import emigroLogo from '@assets/images/emigro-logo.png';
 
 const Header: React.FC = () => {
+  // https://reactnavigation.org/docs/handling-safe-area/
+  const insets = useSafeAreaInsets();
   const navigation = useNavigation();
 
   return (
-    <Box bg="$primary500" pt="$8" pb="$2">
+    <Box bg="$primary500" style={{ paddingTop: insets.top }} pb="$2">
       <Center>
         <Pressable onPress={() => navigation.navigate('Wallet' as never)}>
           <Box w="$40" h="$12">
