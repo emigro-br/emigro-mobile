129a9328fca65e0205b2cd6d4957f98bae507056
Osvaldo Matos Junior <tupy@riachao.com>
2024-03-26 09:03:23 -0300

    Checking for expo updates on app startup
    https://medium.com/@caleb_23647/expo-updates-fa846ce96640

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { useEffect } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import { NavigationContainer } from '@react-navigation/native';
 
 import { GluestackUIProvider } from '@gluestack-ui/themed';
 import { StatusBar } from 'expo-status-bar';
+import * as Updates from 'expo-updates';
 import { observer } from 'mobx-react-lite';
 
 import { config } from '@config/gluestack-ui.config';
@@ -14,8 +16,26 @@ import RootStack from '@navigation/RootStack';
 
 import { SplashScreen } from '@screens/Splash';
 
+async function onFetchUpdateAsync() {
+  try {
+    const update = await Updates.checkForUpdateAsync();
+    if (update.isAvailable) {
+      await Updates.fetchUpdateAsync();
+      await Updates.reloadAsync();
+    }
+  } catch (error) {
+    console.warn(`Error fetching latest Expo update: ${error}`);
+  }
+}
+
 export default observer(function App() {
   const { isLoading, session } = useSession();
+
+  useEffect(() => {
+    if (!__DEV__) {
+      onFetchUpdateAsync();
+    }
+  }, []);
 
   if (isLoading) {
     // We haven't finished checking for the token yet
