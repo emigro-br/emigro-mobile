1c0c3b3d5c0b21a9eb370f3a5e3e9fe208fd455c
Osvaldo Matos Junior <tupy@riachao.com>
2024-04-01 14:09:07 -0300

    Static pament working with fake api

--- src/screens/payments/ReviewPixPayment.tsx
+++ src/screens/payments/ReviewPixPayment.tsx
@@ -1,14 +1,43 @@
+import { useEffect, useState } from 'react';
+
 import { Box, Button, ButtonText, Card, Center, HStack, Heading, Text, VStack } from '@gluestack-ui/themed';
-import { PixElementType, hasError, parsePix } from 'pix-utils';
+import { PixElementType, StaticPixEmvElements, hasError, parsePix } from 'pix-utils';
+
+import { SuccessModal } from '@components/modals/SuccessModal';
+
+import { PixKey, dictKey, pay } from '@services/pix';
+
+enum TransactionStep {
+  NONE = 'none',
+  // CONFIRM_PAYMENT = 'confirm_payment',
+  PROCESSING = 'processing',
+  SUCCESS = 'success',
+  ERROR = 'error',
+}
 
 type Props = {
   navigation: any;
   route: any;
 };
 
-export const ReviewPixPayment = ({ route }: Props) => {
+export const ReviewPixPayment = ({ navigation, route }: Props) => {
   const brCode = route.params.brCode;
+  const [step, setStep] = useState<TransactionStep>(TransactionStep.NONE);
+  const [pixKey, setPixKey] = useState<PixKey>();
   const pix = parsePix(brCode);
+
+  useEffect(() => {
+    const fetchPixKeyAsync = async (pix: StaticPixEmvElements) => {
+      const result = await dictKey(pix.pixKey);
+      // console.debug('Pix Key:', result);
+      setPixKey(result);
+    };
+
+    // console.debug('Parsed Pix:', pix);
+    if (!hasError(pix) && pix.type === PixElementType.STATIC) {
+      fetchPixKeyAsync(pix);
+    }
+  }, [brCode]);
 
   // check pix is PixError
   if (hasError(pix) || pix.type === PixElementType.INVALID) {
@@ -17,14 +46,48 @@ export const ReviewPixPayment = ({ route }: Props) => {
     return <Text>Error:</Text>;
   }
 
-  // FIXME: load CPF/Institution from API using the pix key
+  // FIXME:
+  const taxId = '012.345.678-90';
+  const description = 'Emigro Payment';
+
+  const handleConfirmPayment = async () => {
+    setStep(TransactionStep.PROCESSING);
+    try {
+      const result = await pay({
+        br_code: brCode,
+        tax_id: taxId,
+        description,
+      });
+      console.debug('Pix payment result:', result);
+
+      if (result.payment_id) {
+        setStep(TransactionStep.SUCCESS);
+      } else {
+        console.error('Error paying Pix');
+        setStep(TransactionStep.ERROR);
+      }
+    } catch (error) {
+      console.debug('Error paying Pix:', error);
+      setStep(TransactionStep.ERROR);
+    }
+  };
+
+  const handleCloseFinishedModal = () => {
+    setStep(TransactionStep.NONE);
+    navigation.navigate('WalletTab');
+  };
 
   return (
     <Box flex={1} bg="$white">
+      <SuccessModal
+        isOpen={step === TransactionStep.SUCCESS}
+        title="Transaction completed"
+        onClose={() => handleCloseFinishedModal()}
+      />
       <VStack p="$4" space="lg">
         <Heading>Review your Pix payment</Heading>
-        {pix.type === PixElementType.STATIC && <StaticPix pix={pix} />}
-        <Button>
+        {pix.type === PixElementType.STATIC && <StaticPix pix={pix} pixKey={pixKey} />}
+        <Button onPress={() => handleConfirmPayment()} isDisabled={step === TransactionStep.PROCESSING}>
           <ButtonText>Confirm Payment</ButtonText>
         </Button>
       </VStack>
@@ -43,7 +106,12 @@ export const ReviewPixPayment = ({ route }: Props) => {
 //   </Box>
 // );
 
-const StaticPix = ({ pix }) => (
+interface StaticPixProps {
+  pix: StaticPixEmvElements;
+  pixKey?: PixKey;
+}
+
+const StaticPix = ({ pix, pixKey }: StaticPixProps) => (
   <VStack space="3xl">
     <Box>
       <Text size="4xl" bold>
@@ -64,18 +132,18 @@ const StaticPix = ({ pix }) => (
     <VStack space="md">
       <HStack justifyContent="space-between">
         <Text bold>CPF/CNPJ:</Text>
-        <Text>????</Text>
+        <Text>{pixKey?.tax_id}</Text>
       </HStack>
       <HStack justifyContent="space-between">
         <Text bold>Institution:</Text>
-        <Text>????</Text>
+        <Text maxWidth="$2/3">{pixKey?.bank_name}</Text>
       </HStack>
       <HStack justifyContent="space-between">
         <Text bold>Pix Key:</Text>
         <Text>{pix.pixKey}</Text>
       </HStack>
       <HStack justifyContent="space-between">
-        <Text bold>Indetifier:</Text>
+        <Text bold>Indentifier:</Text>
         <Text>{pix.txid}</Text>
       </HStack>
     </VStack>
