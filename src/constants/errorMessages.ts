87df318a273ffdcdac8a74d38579a30f74fa64c5
Osvaldo Matos Junior <tupy@riachao.com>
2024-05-07 16:24:33 -0300

    CU-86b0brc4e - Improve Sign in form validations

--- src/constants/errorMessages.ts
+++ src/constants/errorMessages.ts
@@ -1,10 +1,8 @@
 export const SIGNUP_ERROR_MESSAGE = 'Error during sign up. Please try again.';
-export const SIGNIN_ERROR_MESSAGE = 'Error to sign in, please check your credentials.';
 export const INVALID_QR_CODE = 'Invalid QR code, try another one';
 export const QUOTE_NOT_AVAILABLE_ERROR = 'No quotes available for the provided values.';
 export const TRANSACTION_ERROR_MESSAGE = 'Error while sending the transaction. Please try again later.';
 export const GET_USER_BALANCE_ERROR = 'Error fetching user balance. Please try again later.';
-export const SIGN_IN_FIELDS_ERROR = 'Email and password are required';
 export const CONFIRM_ACCOUNT_ERROR = 'Error during confirmation. Please try again.';
 export const REFRESH_SESSION_ERROR = 'Error during session refresh. Try to log in again.';
 export const WRONG_CODE_ERROR = 'Wrong code, please try again';
--- src/screens/__tests__/Login.test.tsx
+++ src/screens/__tests__/Login.test.tsx
@@ -43,7 +43,7 @@ describe('Login screen', () => {
     expect(passwordInput).toBeOnTheScreen();
     expect(forgotPasswordLink).toBeOnTheScreen();
     expect(signInButton).toBeOnTheScreen();
-    expect(signInButton).toHaveAccessibilityState({ disabled: true });
+    // expect(signInButton).toHaveAccessibilityState({ disabled: true });
   });
 
   test('Should call signIn with correct credentials', async () => {
@@ -64,6 +64,44 @@ describe('Login screen', () => {
 
     await waitFor(() => {
       expect(sessionStore.signIn).toHaveBeenCalledWith(email, password);
+    });
+  });
+
+  test('Should show api error', async () => {
+    const error = new Error('Custom API error');
+    jest.spyOn(sessionStore, 'signIn').mockRejectedValueOnce(error);
+    const { getByTestId, getByText } = render(<Login navigation={mockNavigattion} />);
+
+    const email = 'test@test.com';
+    const password = 'testpass';
+
+    fireEvent.changeText(getByTestId('email'), email);
+    fireEvent.changeText(getByTestId('password'), password);
+    fireEvent.press(getByTestId('signin-button'));
+
+    await waitFor(() => {
+      expect(getByText('Custom API error')).toBeOnTheScreen();
+    });
+  });
+
+  test('Should navigate to forgot password screen', async () => {
+    const { getByTestId } = render(<Login navigation={mockNavigattion} />);
+
+    const forgotPasswordLink = getByTestId('forgot-password-link');
+    fireEvent.press(forgotPasswordLink);
+
+    await waitFor(() => {
+      expect(mockNavigattion.push).toHaveBeenCalledWith('PasswordRecovery');
+    });
+  });
+
+  test('Should show form error messages', async () => {
+    const { getByText, getByTestId } = render(<Login navigation={mockNavigattion} />);
+    fireEvent.press(getByTestId('signin-button'));
+
+    await waitFor(() => {
+      expect(getByText('Email is required')).toBeOnTheScreen();
+      expect(getByText('Password is required')).toBeOnTheScreen();
     });
   });
 });
