5faa04ee324af43020df20dffc49c8e072941f68
Osvaldo Matos Junior <tupy@riachao.com>
2024-05-17 17:50:50 -0300

    Fix gray background on regresh wallet

--- src/app/wallet/index.tsx
+++ src/app/wallet/index.tsx
@@ -5,6 +5,7 @@ import { useFocusEffect } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 
 import { Box, ScrollView, VStack } from '@gluestack-ui/themed';
+import * as Haptics from 'expo-haptics';
 import { observer } from 'mobx-react-lite';
 
 import { CreateWallet } from '@/components/CreateWallet';
@@ -38,25 +39,34 @@ const Wallet = observer(({ navigation }: Props) => {
     }
 
     setRefreshing(true);
+    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
     try {
-      await balanceStore.fetchUserBalance().catch(console.warn);
+      await balanceStore
+        .fetchUserBalance({
+          force: true,
+        })
+        .catch(console.warn);
     } finally {
       setRefreshing(false);
     }
   }, [balanceStore.fetchUserBalance, publicKey]);
 
   return (
-    <ScrollView refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} title="Refreshing..." />}>
+    <>
       <Box bg="$primary500" py="$2">
         <OperationButtons navigation={navigation} />
       </Box>
-      <VStack space="lg" p="$4">
-        {balanceStore.userBalance.length > 0 && (
-          <WalletBalances userBalance={balanceStore.userBalance} navigation={navigation} />
-        )}
-        {!publicKey && <CreateWallet />}
-      </VStack>
-    </ScrollView>
+      <ScrollView
+        refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} title="Refreshing..." />}
+      >
+        <VStack space="lg" p="$4">
+          {balanceStore.userBalance.length > 0 && (
+            <WalletBalances userBalance={balanceStore.userBalance} navigation={navigation} />
+          )}
+          {!publicKey && <CreateWallet />}
+        </VStack>
+      </ScrollView>
+    </>
   );
 });
 
