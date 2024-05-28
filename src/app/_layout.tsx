055d7c9b1de923105281f06cb334c8e734902b9b
Osvaldo Matos Junior <tupy@riachao.com>
2024-05-28 12:02:00 -0300

    Enable Sentry

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,14 +1,19 @@
 import { useEffect, useState } from 'react';
 
 import { GluestackUIProvider } from '@gluestack-ui/themed';
-import { Slot, SplashScreen } from 'expo-router';
+import * as Sentry from '@sentry/react-native';
 // import { useColorScheme } from 'react-native';
+import { isRunningInExpoGo } from 'expo';
+import { Slot, SplashScreen, useNavigationContainerRef } from 'expo-router';
 import * as Updates from 'expo-updates';
 
 import { config } from '@/config/gluestack-ui.config';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
+
+// Construct a new instrumentation instance. This is needed to communicate between the integration and React
+const routingInstrumentation = new Sentry.ReactNavigationInstrumentation();
 
 async function onFetchUpdateAsync() {
   try {
@@ -22,8 +27,37 @@ async function onFetchUpdateAsync() {
   }
 }
 
-export default function RootLayout() {
+const sentryDsn = process.env.EXPO_PUBLIC_SENTRY_DSN;
+if (sentryDsn) {
+  Sentry.init({
+    dsn: sentryDsn,
+    debug: __DEV__, // Enable debug in development mode
+    environment: __DEV__ ? 'development' : 'production',
+    sampleRate: 1.0, // Send 100% of events for while developing
+    tracesSampleRate: 0.5,
+    ignoreErrors: ['QueryFailedError'], // expo local development error
+    integrations: [
+      new Sentry.ReactNativeTracing({
+        // Pass instrumentation to be used as `routingInstrumentation`
+        routingInstrumentation,
+        enableNativeFramesTracking: !isRunningInExpoGo(),
+        // ...
+      }),
+    ],
+  });
+}
+
+function RootLayout() {
   const [appIsReady, setAppIsReady] = useState(false);
+  // Capture the NavigationContainer ref and register it with the instrumentation.
+  const ref = useNavigationContainerRef();
+
+  useEffect(() => {
+    if (ref) {
+      routingInstrumentation.registerNavigationContainer(ref);
+    }
+  }, [ref]);
+
   // const [loaded, error] = useFonts({
   //   SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   //   ...FontAwesome.font,
@@ -71,3 +105,5 @@ function RootLayoutNav() {
     </GluestackUIProvider>
   );
 }
+
+export default Sentry.wrap(RootLayout);
