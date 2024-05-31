da23a4bca96e91271d965dedc4990d965f9fe2b0
Osvaldo Matos Junior <tupy@riachao.com>
2024-05-31 17:56:01 -0300

    feat: create CardAssetList

--- src/app/(auth)/(tabs)/wallet/transfers/index.tsx
+++ src/app/(auth)/(tabs)/wallet/transfers/index.tsx
@@ -1,32 +1,30 @@
 import React from 'react';
 
-import { Box, Card, ChevronRightIcon, Heading, Icon, Text, VStack } from '@gluestack-ui/themed';
+import { Box, ChevronRightIcon, Heading, Icon, Text, VStack } from '@gluestack-ui/themed';
 import { useRouter } from 'expo-router';
 
-import { AssetList } from '@/components/AssetList';
+import { CardAssetList } from '@/components/AssetList';
 import { balanceStore } from '@/stores/BalanceStore';
 
 export const Transfers = () => {
   const router = useRouter();
-  const data = balanceStore.currentAssets();
+  const assets = balanceStore.currentAssets();
 
   return (
     <Box flex={1}>
       <VStack p="$4" space="md">
         <Heading size="xl">Send money</Heading>
         <Text>Choose the currency you want to send</Text>
-        <Card variant="flat">
-          <AssetList
-            data={data}
-            trailing={<Icon as={ChevronRightIcon} size="md" />}
-            onPress={(item) =>
-              router.push({
-                pathname: '/wallet/transfers/send',
-                params: { asset: item },
-              })
-            }
-          />
-        </Card>
+        <CardAssetList
+          data={assets}
+          trailing={<Icon as={ChevronRightIcon} size="md" />}
+          onPress={(item) =>
+            router.push({
+              pathname: '/wallet/transfers/send',
+              params: { asset: item },
+            })
+          }
+        />
       </VStack>
     </Box>
   );
--- src/components/AssetList.tsx
+++ src/components/AssetList.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Box, FlatList, Pressable } from '@gluestack-ui/themed';
+import { Box, Card, FlatList, Pressable } from '@gluestack-ui/themed';
 
 import { CryptoOrFiat } from '@/types/assets';
 
@@ -13,16 +13,28 @@ type Props = {
 };
 
 export const AssetList = ({ data, trailing, onPress }: Props) => {
+  const renderItem = ({ item }) => (
+    <Pressable onPress={() => onPress(item)}>
+      <AssetListTile asset={item} subasset={item} trailing={trailing} />
+    </Pressable>
+  );
+
   return (
     <FlatList
       data={data}
-      renderItem={({ item }: { item: CryptoOrFiat }) => (
-        <Pressable onPress={() => onPress(item)}>
-          <AssetListTile asset={item} subasset={item} trailing={trailing} />
-        </Pressable>
-      )}
-      keyExtractor={(item: CryptoOrFiat) => item}
+      renderItem={renderItem}
+      keyExtractor={(item) => item as string}
       ItemSeparatorComponent={() => <Box h="$4" />}
     />
   );
 };
+
+type CardProps = Props & {
+  variant?: 'flat' | 'outline' | 'elevated' | 'ghost' | 'filled' | undefined;
+};
+
+export const CardAssetList = ({ variant = 'flat', data, trailing, onPress }: CardProps) => (
+  <Card variant={variant}>
+    <AssetList data={data} trailing={trailing} onPress={onPress} />
+  </Card>
+);
