f00fbdb359ea207e4ee3fbbaa02c16a88e4b22bc
Osvaldo Matos Junior <tupy@riachao.com>
2024-06-28 10:57:47 -0300

    chore: disable Sentry debug

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -31,7 +31,7 @@ const sentryDsn = process.env.EXPO_PUBLIC_SENTRY_DSN;
 if (sentryDsn) {
   Sentry.init({
     dsn: sentryDsn,
-    debug: __DEV__, // Enable debug in development mode
+    debug: false, // __DEV__, // Enable debug in development mode
     environment: __DEV__ ? 'development' : 'production',
     sampleRate: 1.0, // Send 100% of events for while developing
     tracesSampleRate: 0.5,
