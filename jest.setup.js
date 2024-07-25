0dd2b37c7245c3ffeda0648bdb0f64f8a259b533
Osvaldo Matos Junior <tupy@riachao.com>
2024-07-25 18:47:30 -0300

    fix: toast test

--- jest.setup.js
+++ jest.setup.js
@@ -6,5 +6,6 @@ jest.mock('react-native-safe-area-context', () => mockSafeAreaContext);
 jest.spyOn(Keyboard, 'dismiss');
 
 jest.mock('@/components/ui/toast', () => ({
+  ...jest.requireActual('@/components/ui/toast'),
   useToast: jest.fn().mockReturnValue({ show: jest.fn() }),
 }));
