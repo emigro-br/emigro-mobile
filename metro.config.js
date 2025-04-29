5701ace60a457e2f6deaad144236d15ba6a3197a
pongl <hidekikanazawa94@gmail.com>
2025-04-29 13:59:23 +0100

    Packages and Metro compatibility fixed

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,13 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
+const path = require("path");
 
 const config = getDefaultConfig(__dirname);
+
+// 🛠️ Add this block to fake "react-dom"
+config.resolver.extraNodeModules = {
+  ...(config.resolver.extraNodeModules || {}),
+  "react-dom": path.resolve(__dirname, "emptyModule.js"),
+};
 
 module.exports = withNativeWind(config, { input: "./global.css" });
