6d5f2f4174009e5236f42aacc022a466a643a89c
Pongl <hidekikanazawa94@gmail.com>
2025-06-11 21:43:23 +0100

    Emigro v2.2.1.9

--- metro.config.js
+++ metro.config.js
@@ -1,15 +1,10 @@
-const { withNativeWind } = require("nativewind/metro");
-const exclusionList = require("metro-config/src/defaults/exclusionList");
-const path = require("path");
-const {
-  getSentryExpoConfig
-} = require("@sentry/react-native/metro");
-
+const { getSentryExpoConfig } = require('@sentry/react-native/metro');
+const { withNativeWind } = require('nativewind/metro');
+const exclusionList = require('metro-config/src/defaults/exclusionList');
+const path = require('path');
 const config = getSentryExpoConfig(__dirname);
-
-// ✅ Completely block react-native-svg from being loaded on web
 if (process.argv.includes('--web')) {
-  console.log("✅ Blocking react-native-svg for web");
+  console.log('✅ Blocking react-native-svg for web');
   config.resolver.blockList = exclusionList([
     /node_modules\/react-native-svg\/.*/,
   ]);
@@ -17,15 +12,15 @@ if (process.argv.includes('--web')) {
 
 config.resolver.extraNodeModules = {
   ...(config.resolver.extraNodeModules || {}),
-  crypto: require.resolve("crypto-browserify"),
-  stream: require.resolve("stream-browserify"),
-  assert: require.resolve("assert"),
-  buffer: require.resolve("buffer"),
-  process: require.resolve("process/browser"),
-  util: require.resolve("util"),
-  "react-dom": path.resolve(__dirname, "emptyModule.js"),
-  url: path.resolve(__dirname, "emptyModule.js"),
-  axios: path.resolve(__dirname, "shims", "axios.js"),
+  crypto: require.resolve('crypto-browserify'),
+  stream: require.resolve('stream-browserify'),
+  assert: require.resolve('assert'),
+  buffer: require.resolve('buffer'),
+  process: require.resolve('process/browser'),
+  util: require.resolve('util'),
+  'react-dom': path.resolve(__dirname, 'emptyModule.js'),
+  url: path.resolve(__dirname, 'emptyModule.js'),
+  axios: path.resolve(__dirname, 'shims', 'axios.js'),
 };
 
-module.exports = withNativeWind(config, { input: "./global.css" });
+module.exports = withNativeWind(config, { input: './global.css' });
